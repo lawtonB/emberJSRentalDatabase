53dd9688f8ff7057874531efe73ef10272d5b763
erik tolentino and lawton browning <lawton@>
2016-03-31 10:37:50 -0700

    update update function for multiple classes

--- app/components/rental-tile.js
+++ app/components/rental-tile.js
@@ -12,11 +12,11 @@ export default Ember.Component.extend({
     },
     update(rental, params) {
       this.sendAction('update', rental, params);
-    },
-    delete(rental) {
-      if (confirm('are you sure you want to delete this rental?')){
-        this.sendAction('destroyRental', rental);
-      }
     }
+    // delete(rental) {
+    //   if (confirm('are you sure you want to delete this rental?')){
+    //     this.sendAction('destroyRental', rental);
+    //   }
+    // }
   }
 });
--- app/routes/rental.js
+++ app/routes/rental.js
@@ -4,4 +4,22 @@ export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('rental', params.rental_id);
   },
+  actions: {
+    delete(rental) {
+      if (confirm('are you sure you want to delete this rental?')){
+          rental.destroyRecord();
+          this.transitionTo('index');
+        }
+      },
+
+      update(rental, params){
+        Object.keys(params).forEach(function(key){
+          if(params[key]!==undefined) {
+            rental.set(key,params[key]);
+          }
+        });
+        rental.save();
+        // this.transitionTo('index');
+      }
+    }
 });
