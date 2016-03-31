f77161b57ee1383890298d0b78a2929d27be9a42
erik tolentino and lawton browning <lawton@>
2016-03-31 09:34:31 -0700

    add one to many relationship between city and rentals

--- app/routes/index.js
+++ app/routes/index.js
@@ -13,11 +13,11 @@ export default Ember.Route.extend({
 
   actions: {
 
-    save3(params) {
-      var newRental = this.store.createRecord('rental', params);
-      newRental.save();
-      this.transitionTo('index');
-    },
+    // save3(params) {
+    //   var newRental = this.store.createRecord('rental', params);
+    //   newRental.save();
+    //   this.transitionTo('index');
+    // },
 
     update(rental, params){
       Object.keys(params).forEach(function(key){
@@ -41,7 +41,6 @@ export default Ember.Route.extend({
       rental.destroyRecord();
       this.transitionTo('index');
     },
-
     saveCity(params) {
       var newCity = this.store.createRecord('city', params);
       newCity.save();
