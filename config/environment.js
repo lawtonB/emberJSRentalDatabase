a3a2c14d2724933bd0a227f2c9427eae5e629ff3
lawtonB <lawton.a.browning@gmail.com>
2016-04-04 08:46:54 -0700

    add cost and popularity helpers

--- config/environment.js
+++ config/environment.js
@@ -5,7 +5,7 @@ module.exports = function(environment) {
     modulePrefix: 'super-rentals',
     environment: environment,
     contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
-    firebase: 'https://emberrental.firebaseio.com',
+    firebase: 'https://superrentalslbja.firebaseio.com',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
